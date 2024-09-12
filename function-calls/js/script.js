2a0c2a07c4c4f2aee99b51a1f6f547f4f18d0620
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-12 10:23:30 -0400

    Added instruction level comments

--- function-calls/js/script.js
+++ function-calls/js/script.js
@@ -14,9 +14,10 @@
 "use strict";
 
 /**
- * Create a 640 x 480 Pink canvas
+ * Create a 640 x 480 canvas
 */
 function setup() {
+    // Standard background size
     createCanvas(640, 480);
 }
 
@@ -25,6 +26,8 @@ function setup() {
  *Draw a white rectangle on the canvas
 */
 function draw() {
+    // Pink background
     background(255, 100, 100);
+    // White rectangle
     rect(200, 80, 240, 320);
 }
