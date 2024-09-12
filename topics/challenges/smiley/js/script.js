9e7041e517c93352dccc8df4db75e032b9276910
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-12 16:07:20 -0400

    Made smiley challenge

--- topics/challenges/smiley/js/script.js
+++ topics/challenges/smiley/js/script.js
@@ -1,8 +1,8 @@
 /**
- * Title of Project
+ * Smiley Challenge
  * Ellie "DASH" Desjardins
  * 
- * Description of the project
+ * A classic black smiley on a yellow background
  *
  */
 
@@ -12,13 +12,40 @@
  * Setup of the project
 */
 function setup() {
-
+    createCanvas(800, 800);
+    background("yellow")
 }
 
 
 /**
- * Draws OBJECT
+ * Draws smiley
 */
 function draw() {
+    //Face contour
+    push();
+    strokeWeight(20);
+    noFill();
+    ellipse(400, 400, 600);
+    pop();
 
+    // Eye left
+    push();
+    noStroke();
+    fill("#000000");
+    ellipse(300, 300, 100, 200);
+    pop();
+
+    // Eye right
+    push();
+    noStroke();
+    fill("#000000");
+    ellipse(500, 300, 100, 200);
+    pop();
+
+    // Smile
+    push();
+    strokeWeight(20);
+    noFill();
+    arc(400, 400, 400, 400, 0, PI);
+    pop();
 }
