3a8a0683e7f8ebb4470c824b04614b3fbd217a8f
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-12 09:57:19 -0400

    Changed BG colour, added rectangle

--- function-calls/js/script.js
+++ function-calls/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
@@ -20,5 +20,6 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
 }
