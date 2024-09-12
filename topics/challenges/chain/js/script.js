5f7ee082fa78fd8b7ffa0045ba2ef7c172d30e18
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-12 15:58:28 -0400

    Created chain

--- topics/challenges/chain/js/script.js
+++ topics/challenges/chain/js/script.js
@@ -1,24 +1,68 @@
 /**
- * Title of Project
+ * Chain Challenge
  * Ellie "DASH" Desjardins
  * 
- * Description of the project
+ * Black chain on yellow background
  *
  */
 
 "use strict";
 
 /**
- * Setup of the project
+ * Setup of the project and create the canvas
 */
 function setup() {
-
+    createCanvas(800, 800);
 }
 
-
 /**
- * Draws OBJECT
+ * Sets background, draw chain
 */
 function draw() {
+    // Background
+    background("yellow");
+
+    // Chain
+    drawChain();
+}
+
+/**
+ * Draws chain
+ */
+function drawChain(){
+    // Middle chain link
+    push();
+    strokeWeight(40);
+    noFill();
+    ellipse(400, 400, 200, 300);
+    pop();
+
+    // Up 1 chain link
+    push();
+    strokeWeight(40);
+    noFill();
+    ellipse(400, 200, 200, 300);
+    pop();
+
+    // Up 2 chain link
+    push();
+    strokeWeight(40);
+    noFill();
+    ellipse(400, 0, 200, 300);
+    pop();
+
+    // Down 1 chain link
+    push();
+    strokeWeight(40);
+    noFill();
+    ellipse(400, 600, 200, 300);
+    pop();
+
+    // Down 2 chain link
+    push();
+    strokeWeight(40);
+    noFill();
+    ellipse(400, 800, 200, 300);
+    pop();
 
 }
