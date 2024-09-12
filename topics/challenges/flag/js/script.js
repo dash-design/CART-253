bc1e21aa07d1772009ad6813dbebd859268e59af
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-12 15:00:44 -0400

    Started umbrella challenge, did flag challenge

--- topics/challenges/flag/js/script.js
+++ topics/challenges/flag/js/script.js
@@ -1,24 +1,42 @@
 /**
- * Title of Project
+ * Flag challenge
  * Ellie "DASH" Desjardins
  * 
- * Description of the project
+ * Flag challenge for CART 253 week 2
  *
  */
 
 "use strict";
 
 /**
- * Setup of the project
+ * Setup of the canvas and background
 */
 function setup() {
+    createCanvas(800, 800);
+    background("green");
 
 }
 
-
 /**
- * Draws OBJECT
+ * Draws Flag
 */
 function draw() {
+    drawFlagUp();
+    drawFlagDown();
+}
 
+function drawFlagUp(){
+    push();
+	noStroke();
+	fill("blue");
+	rect(200, 225, 400, 150);
+	pop();
+}
+
+function drawFlagDown(){
+    push();
+	noStroke();
+	fill("yellow");
+	rect(200, 375, 400, 150);
+	pop();
 }
--- topics/challenges/umbrella/js/script.js
+++ topics/challenges/umbrella/js/script.js
@@ -2,23 +2,57 @@
  * Title of Project
  * Ellie "DASH" Desjardins
  * 
- * Description of the project
+ * Umbrella challenge
  *
  */
 
 "use strict";
 
 /**
- * Setup of the project
+ * Setup of the project and create canvas
 */
 function setup() {
-
+    //400x400 canvas
+    createCanvas(400, 400);
 }
-
 
 /**
- * Draws OBJECT
+ * Sets background and setup umbrella
 */
 function draw() {
+    // The background
+    background("red");
 
+    // The umbrella
+    drawUmbrella();
 }
+
+/**
+ * Draws an umbrella
+ */
+function drawUmbrella() {
+    // stick
+    push();
+    strokeweight(10);
+    stroke(200, 200, 200, 300);
+    pop();
+
+    // handle
+    push();
+    noFill();
+    stroke("yellow");
+    strokeweight(10);
+    arc(220, 300,40, 60, 0, PI);
+    pop();
+
+    //Umbrella
+    push();
+    pop();
+}
+
+// /**
+//  * Draws OBJECT
+// */
+// function draw() {
+
+// }
