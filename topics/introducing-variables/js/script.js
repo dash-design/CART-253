975420bebc315580842a3cea9c230f871c9b7ac4
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-18 14:50:19 -0400

    Moved learning material for week of September 16 into repository

---
+++ topics/introducing-variables/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Title of Project
+ * Ellie "DASH" Desjardins
+ * 
+ * Description of the project
+ *
+ */
+
+"use strict";
+
+/**
+ * Setup of the project
+*/
+function setup() {
+
+}
+
+
+/**
+ * Draws OBJECT
+*/
+function draw() {
+
+}
