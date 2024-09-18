b506dc6ebfc7ad49c3a31b1b159b2fad442d5849
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-18 17:02:51 -0400

    Creates circles with variables

--- topics/introducing-variables/js/script.js
+++ topics/introducing-variables/js/script.js
@@ -9,16 +9,46 @@
 "use strict";
 
 /**
- * Setup of the project
+ * Creates a canvas
 */
 function setup() {
-
+    createCanvas(800, 480);
 }
 
 
 /**
- * Draws OBJECT
+ * Draws circles in the centre of the canvas
 */
 function draw() {
+    background(0);
+
+    //Draws the circle in the middle of the canvas that changes colour according to the mouse position
+    push();
+    fill(mouseX, mouseY, 0);
+    noStroke();
+    ellipse(width / 2, height / 2, 100, 100);
+    pop();
+
+    //Draws the cyan circle according to the position of the mouse
+    push();
+    fill(0, 255, 255);
+    noStroke();
+    ellipse(mouseX, mouseY, 100, 100);
+    pop();
+
+    //Draws the magenta circle opposite to the position of the mouse
+    push();
+    fill(255, 0, 255);
+    noStroke();
+    ellipse(mouseY, mouseX, 100, 100);
+    pop();
+
+    //Draws the white ellipse in the middle of the canvas that changes size according to mouse position
+    push();
+    noFill();
+    stroke(255, 255, 255);
+    strokeWeight(10);
+    ellipse(width / 2, height / 2, mouseY, mouseX);
+    pop();
 
 }
