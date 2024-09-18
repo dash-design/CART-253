9988e413760d7502d711a2f13df5948b3dfc5306
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-18 18:29:39 -0400

    Added a model of velocity and acceleration to the program

--- topics/movement-variables/js/script.js
+++ topics/movement-variables/js/script.js
@@ -8,17 +8,59 @@
 
 "use strict";
 
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50,
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    minVelocity: {
+        x: -3,
+        y: -2
+    },
+    maxVelocity: {
+        x: 3,
+        y: 2
+    },
+    acceleration: {
+        x: 0.025,
+        y: -0.05
+    }
+};
+
 /**
- * Setup of the project
+ * Creates the canvas
 */
 function setup() {
+    createCanvas(640, 480);
 
 }
 
 
 /**
- * Draws OBJECT
+ * Moves the bird and displays it
 */
 function draw() {
+    background(0);
+
+    //Move the bird
+    bird.velocity.x += bird.acceleration.x;
+    //+= is the same as "= bird.velocity.y +"
+    bird.velocity.y = bird.velocity.y + bird.acceleration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    bird.x = bird.x + bird.velocity.x;
+    bird.y = bird.y + bird.velocity.y;
+
+    //Draw the bird
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size);
+    pop();
 
 }
