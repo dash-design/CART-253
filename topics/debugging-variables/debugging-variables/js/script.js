6b01b7d899a8b639b70063dc052110d963d69a1f
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-18 18:50:11 -0400

    Debugged the bugged bug

--- topics/debugging-variables/debugging-variables/js/script.js
+++ topics/debugging-variables/debugging-variables/js/script.js
@@ -8,29 +8,31 @@
 
 "use strict";
 
-const bgColor = "#87ceeb"; // Sky blue
+const bgColour = "#87ceeb"; // Sky blue
 
 // The bug
 const bug = {
     // Position and dimensions
-    x: 250
+    x: 250,
     y: 0,
     w: 20,
+    h: 30,
     // Movement
     velocity: {
-        x: -100,
-        y: -12, // Starts moving down
+        x: 0,
+        y: 5, // Starts moving down
     },
     // Colour
-    color: "#87ceeb" // Dark brown
+    color: "#874444" // Dark brown
+};
 
 
 /**
  * Create the canvas
 */
 function setup() {
-        createCanvas(500, 500);
-}
+    createCanvas(500, 500);
+};
 
 
 /**
@@ -41,12 +43,12 @@ function draw() {
 
     moveBug();
     drawBug();
-}
+};
 
 function moveBug() {
-    bug.xx = bug.x + bug.velocity.x;
-    bug.y == bug.y + bug.velocity.y;
-}
+    bug.x = bug.x + bug.velocity.x;
+    bug.y = bug.y + bug.velocity.y;
+};
 
 /**
  * Displays the bug with its six legs sticking out
@@ -56,12 +58,12 @@ function drawBug() {
     push();
     noStroke();
     fill(bug.color);
-    ellipse(bug.x, bug.y, bug.width, bug.height);
+    ellipse(bug.x, bug.y, bug.w, bug.h);
     pop();
 
     // Legs
     push();
-    stroke(bug.stroke);
+    stroke(bug.color);
     // Thicken the legs a bit
     strokeWeight(2);
     // Three lines horizontally across the body at different heights for the legs
@@ -69,4 +71,4 @@ function drawBug() {
     line(bug.x - bug.w, bug.y, bug.x + bug.w, bug.y);
     line(bug.x - bug.w, bug.y + bug.h / 4, bug.x + bug.w, bug.y + bug.h / 4);
     pop();
-}
+};
