de374687adfb34e759cf6606930c53b8dc6807cb
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-18 18:55:07 -0400

    Copy pasted Mr.Furious code

--- topics/challenges/mr_furious/js/script.js
+++ topics/challenges/mr_furious/js/script.js
@@ -1,24 +1,44 @@
 /**
- * Title of Project
+ * Mr.Furious Challenge
  * Ellie "DASH" Desjardins
  * 
- * Description of the project
+ * Septembre 19 challenge with our friend Mr.Furious to practice using variables
  *
  */
 
 "use strict";
 
-/**
- * Setup of the project
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+    // Position and size
+    x: 200,
+    y: 200,
+    size: 100,
+    // Colour
+    fill: {
+        r: 255,
+        g: 225,
+        b: 225
+    }
+};
 
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
 }
 
-
 /**
- * Draws OBJECT
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
+    background(160, 180, 200);
 
+    // Draw Mr. Furious as a coloured circle
+    push();
+    noStroke();
+    fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+    ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    pop();
 }
