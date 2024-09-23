34933c7edb5206f5916f28a0b77a9e535185fc27
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-23 16:19:22 -0400

    Added notes to art-jam project

--- topics/assignments/art-jam/js/script.js
+++ topics/assignments/art-jam/js/script.js
@@ -16,12 +16,28 @@ function setup() {
 }
 
 // Variables to change something (movement, scale, colour, etc.)
+// Sun position = changes colour of sky, pyramid, sand dunes, and size and shade of pyramid' shadow
 
 // Use "mouseX" & "mouseY" to allow user interaction/input
+// MouseX = Sun position = timeOfDay
 
 // Use "map()"
+// MouseX moves sun in the sky = timeOfDay
 
 // Use min. one conditional
+// If mouseX at < 1/2 of canvas = morning, then bright colours. else mouseX at > 1/2 canvas = evening/night, then dark colours
+
+
+// The sky that changes colour according to time of day (sun's position)
+
+// The sand dunes that change colour according to timeOfDay
+
+// The pyramid that changes shade according to timeOfDay
+
+// The sun that moves according to mouse position
+
+// The pyramid shadow that changes according to timeOfDay
+
 
 /**
  * Draws OBJECT
