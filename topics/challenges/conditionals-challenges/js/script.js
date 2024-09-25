f980f9ff66177f5234e700fc28d7abeb303ad98f
dash-design <164562752+dash-design@users.noreply.github.com>
2024-09-25 16:55:01 -0400

    Started challenge 1

--- topics/challenges/conditionals-challenges/js/script.js
+++ topics/challenges/conditionals-challenges/js/script.js
@@ -1,24 +1,76 @@
 /**
- * Title of Project
- * Ellie "DASH" Desjardins
- * 
- * Description of the project
+ * Circle Master
+ * Pippin Barr
  *
+ * This will be a program in which the user can move a circle
+ * on the canvas using their own circle to "lead" it around.
  */
 
-"use strict";
+const puck = {
+    x: 350,
+    y: 350,
+    size: 100,
+    fill: "#ff0000"
+};
+
+const user = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
+    size: 75,
+    fill: "#000000"
+};
 
 /**
- * Setup of the project
-*/
+ * Create the canvas
+ */
 function setup() {
+    createCanvas(400, 400);
+}
+
+function movetarget() {
 
 }
 
+/**
+ * Move the user circle, check for overlap, draw the two circles
+ */
+function draw() {
+    background("#aaaaaa");
+
+    // Move user circle
+    moveUser();
+
+    // Draw the user and puck
+    drawUser();
+    drawPuck();
+}
 
 /**
- * Draws OBJECT
-*/
-function draw() {
+ * Sets the user position to the mouse position
+ */
+function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
+}
 
+/**
+ * Displays the user circle
+ */
+function drawUser() {
+    push();
+    noStroke();
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+    pop();
+}
+
+/**
+ * Displays the puck circle
+ */
+function drawPuck() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(puck.x, puck.y, puck.size);
+    pop();
 }
