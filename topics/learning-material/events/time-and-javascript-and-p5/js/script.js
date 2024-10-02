431939c11313cad66e5785182b4ecf684fa579a6
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-02 18:39:14 -0400

    Created example

--- topics/learning-material/events/time-and-javascript-and-p5/js/script.js
+++ topics/learning-material/events/time-and-javascript-and-p5/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Title of Project
+ * Time and JavaScript and p5
  * Ellie "DASH" Desjardins
  * 
  * Description of the project
@@ -7,18 +7,40 @@
  */
 
 "use strict";
-
+// // Basic of JavaScript
+// let x = 10;
+// let y = 11;
+// let z = x * y;
+// But then you add more complex ideas, like constant
+const ball = {
+    x: 0,
+    y: 200,
+    size: 50
+};
+// Then setup the project
 /**
  * Setup of the project
 */
 function setup() {
-
+    // Often starts with the canvas here
+    createCanvas(400, 400);
 }
 
-
+// Then the draw function
 /**
  * Draws OBJECT
 */
 function draw() {
+    // The colour of the background
+    background(0);
+    // Drawing the ball
+    // Starting from its position
+    ball.x += 1;
 
+    // Push first
+    push();
+    noStroke();
+    ellipse(ball.x, ball.y, ball.size);
+    // And pop at the end
+    pop();
 }
