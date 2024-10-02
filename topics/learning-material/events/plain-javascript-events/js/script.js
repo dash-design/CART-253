c2935b58ef23f82cdb0df108998d79d00f5d6331
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-02 19:59:30 -0400

    Started plain JavaScript Events project

---
+++ topics/learning-material/events/plain-javascript-events/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Plain JavaScript Events
+ * Ellie "DASH" Desjardins
+ * 
+ * Description of the project
+ *
+ */
+
+"use strict";
+
+/**
+ * Setup of the project
+*/
+function setup() {
+
+}
+
+
+/**
+ * Draws OBJECT
+*/
+function draw() {
+
+}
