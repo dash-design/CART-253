1295f0b074895a9bcbf7b106a9e4a080187c436b
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-02 18:40:55 -0400

    Started introducing-events

---
+++ topics/learning-material/events/introducing-events/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Introducing events
+ * Ellie "DASH" Desjardins
+ * 
+ * Description of the project
+ *
+ */
+
+"use strict";
+
+/**
+ * Setup of the project
+*/
+function setup() {
+
+}
+
+
+/**
+ * Draws OBJECT
+*/
+function draw() {
+
+}
