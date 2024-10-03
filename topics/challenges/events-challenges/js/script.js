a60c4af4816633f666144784fdd6d32e67c62578
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-03 13:46:23 -0400

    completed challenge 1 in class

--- topics/challenges/events-challenges/js/script.js
+++ topics/challenges/events-challenges/js/script.js
@@ -1,24 +1,75 @@
 /**
- * Title of Project
- * Ellie "DASH" Desjardins
- * 
- * Description of the project
+ * The Only Move Is Not To Play
+ * Pippin Barr
  *
+ * A game where your score increases so long as you do nothing.
  */
 
 "use strict";
 
-/**
- * Setup of the project
-*/
-function setup() {
+// Current score
+let score = 0;
 
+// Is the game over?
+let gameOver = false;
+
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
 }
 
+/**
+ * Update the score and display the UI
+ */
+function draw() {
+    background("#87ceeb");
+
+    // Only increase the score if the game is not over
+    if (!gameOver) {
+        // Score increases relatively slowly
+        score += 0.05;
+    }
+    displayUI();
+}
 
 /**
- * Draws OBJECT
-*/
-function draw() {
+ * Show the game over message if needed, and the current score
+ */
+function displayUI() {
+    if (gameOver) {
+        push();
+        textSize(48);
+        textStyle(BOLD);
+        textAlign(CENTER, CENTER);
+        text("You lose!", width / 2, height / 3);
+        pop();
+    }
+    displayScore();
+}
 
+/**
+ * Display the score
+ */
+function displayScore() {
+    push();
+    textSize(48);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    text(floor(score), width / 2, height / 2);
+    pop();
+}
+
+// Challenge 1, attempt 1
+function keyPressed(event) {
+    endGame();
+}
+
+function keyReleased(event) {
+    endGame();
+}
+
+function endGame() {
+    gameOver = true
 }
