aebad07ae9507f0aa9811fae5b0f44dde6734af1
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-02 20:19:26 -0400

    Added a keydown handler for background changing colour

--- topics/learning-material/events/plain-javascript-events/js/script.js
+++ topics/learning-material/events/plain-javascript-events/js/script.js
@@ -2,23 +2,50 @@
  * Plain JavaScript Events
  * Ellie "DASH" Desjardins
  * 
- * Description of the project
+ * Experimenting with events handling in plain JavaScript
  *
  */
 
 "use strict";
 
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#fffff"
+    },
+    switchKey: 32 // Spacebar
+};
+
 /**
- * Setup of the project
+ * Creates the canvas
 */
 function setup() {
+    createCanvas(400, 400);
 
+    // Listen to keypresses
+    window.addEventListener("keydown", changeBG);
 }
 
 
 /**
- * Draws OBJECT
+ * Displays the background
 */
 function draw() {
+    background(bg.fill);
+}
+
+
+// Switches the background from black to white
+function changeBG(event) {
+    if (event.keyCode === bg.switchKey) {
+        if (bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        }
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
 
 }
+
