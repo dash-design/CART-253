cf979fdd60dd003e24c5b601bf08ee57b5b1d0f8
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-24 14:06:40 -0400

    Factored the function challenge, implemented movePaddle, moveBall, handleBounce, and added gravity

--- topics/challenges/functions-challenges/js/script.js
+++ topics/challenges/functions-challenges/js/script.js
@@ -12,11 +12,12 @@
 const ball = {
     x: 300,
     y: 20,
+    fill: "red",
     width: 10,
     height: 10,
     velocity: {
         x: 0,
-        y: 0
+        y: 5
     }
 };
 
@@ -24,15 +25,21 @@ const ball = {
 const paddle = {
     x: 300,
     y: 280,
+    fill: "black",
     width: 80,
     height: 10
 };
+
+// The gravity
+const gravity = 0.1;
 
 /**
  * Create the canvas
 */
 function setup() {
     createCanvas(600, 300);
+
+    // frameRate(20);
 }
 
 
@@ -47,48 +54,60 @@ function draw() {
 
     handleBounce(ball, paddle);
 
-    drawPaddle(paddle);
-    drawBall(ball);
+    drawBlock(paddle);
+    drawBlock(ball);
 }
 
 /**
  * Moves the paddle
  */
 function movePaddle(paddle) {
-
+    paddle.x = mouseX; // Remember this
 }
 
 /**
  * Moves the ball
  */
 function moveBall(ball) {
+    ball.velocity.y = ball.velocity.y + gravity; // Gravity
+
+    ball.y = ball.y + ball.velocity.y; // Velocity formula to remember
 
 }
 
 function handleBounce(ball, paddle) {
+    const overlap = centeredRectanglesOverlap(ball, paddle);
+    if (overlap) {
+        ball.y = paddle.y - paddle.height / 2 - ball.width / 2;
+        // ball.velocity.y *= -1;
+        ball.velocity.y = -ball.velocity.y; // Same thing as the one above
+
+    }
 
 }
 
 /**
- * Draws the paddle on the canvas
+ * Returns true if a and b overlap, and false otherwise
+ * Assumes a and b have properties x, y, width and height to describe
+ * their rectangles, and that a and b are displayed centred on their
+ * x,y coordinates.
  */
-function drawPaddle(paddle) {
-    push();
-    rectMode(CENTER);
-    noStroke();
-    fill("pink");
-    rect(paddle.x, paddle.y, paddle.width, paddle.height);
-    pop();
+function centeredRectanglesOverlap(a, b) {
+    return (a.x + a.width / 2 > b.x - b.width / 2 &&
+        a.x - a.width / 2 < b.x + b.width / 2 &&
+        a.y + a.height / 2 > b.y - b.height / 2 &&
+        a.y - a.height / 2 < b.y + b.height / 2);
 }
 
+
 /**
- * Draws the ball on the canvas
+ *  Draws the game blocks
  */
-function drawBall(ball) {
+function drawBlock(block) {
     push();
     rectMode(CENTER);
     noStroke();
-    fill("pink");
-    rect(ball.x, ball.y, ball.width, ball.height);
+    fill(block.fill);
+    rect(block.x, block.y, block.width, block.height);
     pop();
 }
