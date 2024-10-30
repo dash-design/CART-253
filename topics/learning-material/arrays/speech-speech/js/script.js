95624918252908d0758826c83d585157448d353d
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-30 19:33:09 -0400

    Finished speech! speech!

--- topics/learning-material/arrays/speech-speech/js/script.js
+++ topics/learning-material/arrays/speech-speech/js/script.js
@@ -1,24 +1,50 @@
 /**
- * Title of Project
+ * Speech! Speech!
  * Ellie "DASH" Desjardins
  * 
- * Description of the project
+ * Interactice speech-playing interface
  *
  */
 
 "use strict";
 
+// The speech itself
+const speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+// Which element of the speech to display
+let speechIndex = 0;
+
 /**
- * Setup of the project
+ * Creates canvas
 */
 function setup() {
+    createCanvas(600, 100);
 
 }
 
-
 /**
- * Draws OBJECT
+ * Displays the current element of the speech
 */
 function draw() {
+    background(0);
 
+    // Gets the current element of the speech
+    let currentElement = speech[speechIndex];
+
+    // Displays the element
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentElement, width / 2, height / 2);
+    pop();
+}
+
+// Changes to the next element on mouse click
+function mousePressed() {
+    // Next line
+    speechIndex = speechIndex + 1;
+    // Handles th end of the speech
+    if (speechIndex >= speech.length) {
+        speechIndex = 0;
+    }
 }
