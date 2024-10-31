719c3917b50406118fa50adb60e7ab64bcef490c
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-31 14:19:18 -0400

    Finished arrays challenges

--- topics/challenges/arrays-challenges/js/script.js
+++ topics/challenges/arrays-challenges/js/script.js
@@ -1,24 +1,103 @@
 /**
- * Title of Project
- * Ellie "DASH" Desjardins
- * 
- * Description of the project
+ * Boingo
+ * Pippin Barr
  *
+ * A ball that bounces around on the canvas
  */
 
-"use strict";
+
+// All the balls
+const balls = [];
 
 /**
- * Setup of the project
-*/
+ * Create the canvas
+ */
 function setup() {
+    // Create the canvas
+    createCanvas(400, 400);
+}
+
+/**
+ * Creates a random ball
+ */
+function createBall(x, y) {
+    // Create a ball object with appropriate properties
+    const newBall = {
+        // Position and dimensions
+        x: x,
+        y: y,
+        size: 20,
+        // Colour
+        fill: random(100, 255),
+        // Movement
+        velocity: {
+            x: random(-5, 5),
+            y: random(-5, 5)
+        }
+    };
+    return newBall;
+}
+
+/**
+ * Moves and draws the ball
+ */
+function draw() {
+    background("#87ceeb");
+
+    for (let ball of balls) {
+        moveBall(ball);
+        bounceBall(ball);
+        drawBall(ball);
+    }
 
 }
 
+/**
+ * Moves the ball according to its velocity
+ */
+function moveBall(ball) {
+    ball.x += ball.velocity.x;
+    ball.y += ball.velocity.y;
+}
 
 /**
- * Draws OBJECT
-*/
-function draw() {
+ * Bounces the ball off the walls
+ */
+function bounceBall(ball) {
+    // Check if the ball has reached the left or right
+    const bounceX = (ball.x > width || ball.x < 0);
+    // Check if the ball has reached the top or bottom
+    const bounceY = (ball.y > height || ball.y < 0);
+
+    // Handle bouncing horizontally
+    if (bounceX) {
+        ball.velocity.x *= -1;
+    }
+    // Handle bouncing vertically
+    if (bounceY) {
+        ball.velocity.y *= -1;
+    }
+}
+
+/**
+ * Draw the ball on the canvas
+ */
+function drawBall(ball) {
+    push();
+    noStroke();
+    fill(ball.fill);
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
+}
+
+/**
+ * Add new balls on click
+ */
+function mousePressed() {
+    // Set the maximum number of balls
+    if (balls.length < 10) {
+        let newBall = createBall(mouseX, mouseY);
+        balls.push(newBall);
+    }
 
 }
