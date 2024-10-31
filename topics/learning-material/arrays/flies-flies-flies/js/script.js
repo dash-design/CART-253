7ee5d96fd0afc1d2a084d24e213ecdab63e98e75
dash-design <164562752+dash-design@users.noreply.github.com>
2024-10-30 20:08:00 -0400

    Finished  flies flies flies project

--- topics/learning-material/arrays/flies-flies-flies/js/script.js
+++ topics/learning-material/arrays/flies-flies-flies/js/script.js
@@ -9,32 +9,26 @@
 
 // Our array of flies (specifically "fly data" really)
 // Each fly has a position and a size
-let flies = [
-    {
-        x: 100,
-        y: 125,
-        size: 10,
-        buzziness: 2
-    },
-    {
-        x: 160,
-        y: 170,
-        size: 14,
-        buzziness: 4
-    },
-    {
-        x: 180,
-        y: 50,
-        size: 5,
-        buzziness: 6
-    }
-];
+let flies = [];
 
 /**
  * Creates the canvas
  */
 function setup() {
     createCanvas(300, 300);
+}
+
+/**
+ * Retuns a random fly 
+ */
+function createFly() {
+    let fly = {
+        x: random(0, width),
+        y: random(0, height),
+        size: random(5, 20),
+        buzziness: random(2, 8)
+    };
+    return fly;
 }
 
 /**
@@ -68,4 +62,31 @@ function drawFly(fly) {
     fill(0);
     ellipse(fly.x, fly.y, fly.size);
     pop();
+}
+
+/**
+ * Adds a fly every time a key is pressed
+ */
+function keyPressed() {
+    // Creates new fly
+    const newFly = createFly();
+    // Adds it to the array
+    flies.push(newFly);
+}
+
+/**
+ * Deletes a fly every time we click
+ */
+function mousePressed() {
+    // Checks all the flies
+    for (let fly of flies) {
+        const d = dist(mouseX, mouseY, fly.x, fly.y);
+        if (d < fly.size / 2) {
+            // This fly was clicked
+            // Get the index of this fly
+            const index = flies.indexOf(fly);
+            // Splices it out of the array
+            flies.splice(index, 1);
+        }
+    }
 }
