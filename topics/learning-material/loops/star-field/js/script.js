9a2774102e767f8fd205e1803659b8c08d34517d
dash-design <164562752+dash-design@users.noreply.github.com>
2024-11-06 22:35:42 -0500

    finished star field project

--- topics/learning-material/loops/star-field/js/script.js
+++ topics/learning-material/loops/star-field/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Title of Project
+ * Star-Field
  * Ellie "DASH" Desjardins
  * 
  * Description of the project
@@ -8,10 +8,13 @@
 
 "use strict";
 
+const numberStars = 100;
+
 /**
  * Setup of the project
 */
 function setup() {
+    createCanvas(800, 400);
 
 }
 
@@ -20,5 +23,23 @@ function setup() {
  * Draws OBJECT
 */
 function draw() {
+    background(0);
 
+    randomSeed(2);
+    for (let i = 0; i < numberStars; i++) {
+        drawStar();
+    }
+
+}
+
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+
+    push();
+    fill(255);
+    noStroke;
+    ellipse(x, y, diameter);
+    pop();
 }
