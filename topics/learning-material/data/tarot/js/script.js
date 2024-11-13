dac05eea74e79bce1a97ed82410ed20b804f4bff
dash-design <164562752+dash-design@users.noreply.github.com>
2024-11-13 15:37:33 -0500

    added JSON file to tarot project

--- topics/learning-material/data/tarot/js/script.js
+++ topics/learning-material/data/tarot/js/script.js
@@ -7,17 +7,39 @@
 
 "use strict";
 
+// tarot data
+let tarot = undefined;
+
 /**
- * tbd.
+ * Load the tarot data
+ */
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
+}
+
+/**
+ * creates canvas
 */
 function setup() {
+    createCanvas(800, 400);
 
 }
 
-
 /**
- * tbd.
+ * displays tarot
 */
 function draw() {
+    background(0);
+
+    const description = tarot.description;
+
+    // displays the information
+    push();
+    textSize(16);
+    fill("yellow");
+    textALign(CENTER, CENTER);
+    text(description, width / 2, height / 2);
+    pop();
+
 
 }
