c5b7a385ebed8ef0b992817180a99cd9eaae333f
dash-design <164562752+dash-design@users.noreply.github.com>
2024-11-13 15:16:21 -0500

    finished machines project

--- topics/learning-material/data/machines/js/script.js
+++ topics/learning-material/data/machines/js/script.js
@@ -10,6 +10,30 @@
 
 "use strict";
 
+const machines = [{
+    type: "incinerator",
+    x: 0,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#ff4400"
+}, {
+    type: "freezer",
+    x: 150,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#bbbbff"
+}, {
+    type: "crusher",
+    x: 300,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#777777"
+}];
+
+
 /**
  * Create the canvas
  */
@@ -23,24 +47,40 @@ function setup() {
 function draw() {
     background(0);
 
-    // Incinerator
-    push();
-    noStroke();
-    fill("#ff4400");
-    rect(0, 100, 100, 100);
-    pop();
+    for (let machine of machines) {
+        drawMachine(machine);
+    }
 
-    // Freezer
-    push();
-    noStroke();
-    fill("#bbbbff");
-    rect(150, 100, 100, 100);
-    pop();
+    // drawMachine(incinerator);
+    // drawMachine(freezer);
+    // drawMachine(crusher);
 
-    // Crusher
+    // // Incinerator
+    // push();
+    // noStroke();
+    // fill(incinerator.fill);
+    // rect(incinerator.x, incinerator.y, incinerator.width, incinerator.height);
+    // pop();
+
+    // // Freezer
+    // push();
+    // noStroke();
+    // fill(freezer.fill);
+    // rect(freezer.x, freezer.y, freezer.width, freezer.height);
+    // pop();
+
+    // // Crusher
+    // push();
+    // noStroke();
+    // fill(crusher.fill);
+    // rect(crusher.x, crusher.y, crusher.width, crusher.height);
+    // pop();
+}
+
+function drawMachine(machine) {
     push();
     noStroke();
-    fill("#777777");
-    rect(300, 100, 100, 100);
+    fill(machine.fill);
+    rect(machine.x, machine.y, machine.width, machine.height);
     pop();
 }
